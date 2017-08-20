3363220e1558e2a17a56f136fb037b5f9e0d652b
aj2109 <adamj2109@gmail.com>
2017-08-20 22:41:15 +0100

    Improved look of exercise selection page

--- public/js/patientsExercises.js
+++ public/js/patientsExercises.js
@@ -1,4 +1,3 @@
-
 $(function() {
 
   const config = {
@@ -9,6 +8,7 @@ $(function() {
     storageBucket: "strokere-c60f6.appspot.com",
     messagingSenderId: "900063170507"
   };
+
   firebase.initializeApp(config);
   database = firebase.database();
   var ref = database.ref();
@@ -39,94 +39,52 @@ $(function() {
           var errorMessage = error.message;
         });
   });
-      //example for the pushing to database, object is the JS variable that we want to send to the
-      //sever . ref is the parent branch
-      //firebase.database().ref('parentbranch').push({
-      //  object
-
+ 
   function patientData(data) {
-    //var patientlistings = selectAll('.patientlisting');
-    //for (var i = 0; i < patientlistings.length ; i++) {
-      //patientlistings[i].remove();
-    //}
-    //console.log(data.val());
-   
-    //console.log(keys);
-    //data is parameter, .val is the value of it (inbuilt function)
-    //you make a variable of patients to hold this data value from console
-    //then you make a variable to hold an object of this to make into array
-    //then console.log puts this array into the console
-    //for loop below start at 0, go through all of them, end when it reaches length
-    //make a variable key to hold they number of the keys
-    //make a variable name, surname, email to hold the patients and their keys
-    //then console puts this all into the console, commented out as want to put into html
-    //the list creates a list using the name surname and email
-    //list.parent then says that this will attach the to the list in html file
     var patients = data.val();
     var keys = Object.keys(patients);
-    //console.log(keys);
     for(var i=0; i< keys.length; i++) {
       var key = keys[i];
       var email = patients[key].email;
       var name = patients[key].name;
       var namelist = [];
       namelist.push(name);
-      //patientChosen(name);
       var surname = patients[key].surname;
-      //console.log(name, surname, email);
-      document.getElementById("patientlist").innerHTML += ("<tr class='pData'>" + "<td id='keycolumn' class='sorting_1'>" +  key +  "</td>" + "<td>" + name + "</td>" + "<td>" + surname + "</td>" +  "<td>" + email + "</td>" + "<th>" +
-            "<form> <button id='buttonname' type='button' class='btnSelect' > Select </button> </form>" + "</th>" + "</tr>");
+      document.getElementById("patientlist").innerHTML += ("<tr class='pData'>" + "<td id='keycolumn' class='sorting_1'>" +  key +  "</td>" + "<td>" + name + "</td>" + "<td>" + surname + "</td>" +  "<td>" + email + "</td>" + "<td>" +
+            "<form> <button id='buttonname' type='button' class='btnSelect' > Select </button> </form>" + "</td>" + "</tr>");
                var tableRows = $('table').find('td#keycolumn').text();
-               console.log(tableRows);
-            
-    //console.log($(this).text());
-}}
+    }
+}
 
 var currentRow;
 var uniqueID;
+var i = 1;
+var a = 0;
 
   $('#patientlist').on('click', '.btnSelect', function() {
      currentRow=$(this).closest("tr");
      uniqueID=currentRow.find("td:eq(0)").text();
      $('.pData').hide();
      $(currentRow).show();
-     console.log(uniqueID);
     });
      
 $('.showPatients').on('click', function() {
      $('.pData').show();
-    });
+});
 
-                      /*$("#"+i+"").on("click", function(){
-                        console.log(name);*/
- 
-/*
-$("#patientlist").on("click", "tr", function(event){
-    alert($(this).index());
-    alert($(this).siblings("td:first").text());
-}); 
-   $("#patientlist").on("click", function(){
-console.log(name);
-     });
-  } */
-//NOTE NOTE NOTE NOTE make new variable called fullName and make it name+surname and then just have that as a columm so can search whole name
-        
-              var i = 1;
-var a = 0;
 function exerciseData(data) {
-  
-refChild.once("value", function(snapshot) {
-  snapshot.forEach(function(child) {
-    if(child.val() != "serialVersionUID"){
-        //console.log(child.key+": "+child.val());
+  refChild.once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+      if(child.val() != "serialVersionUID"){
         var a =  child.val();
         var b = a.replace("_", " ").replace("_", " ").replace("_", " ").replace("_", " ");
-        document.getElementById("exerciselist").innerHTML += ("<tr>" + "<td class='eData'>" + b + "</td>" +  "<th>" +
-            "<form> <input type='checkbox' class='excSelect'> </button> </form>" + "</th>" + "<tr>");  
-    }
+        document.getElementById("exerciselist").innerHTML += ("<tr>" + "<td class='eData'>" + b + "</td>" +  "<th class='css'>" +
+            "<form> <input type='checkbox' class='excSelect'> </button> </form>" + "</th>" + "</tr>");  
+      }
+    });
   });
-});
 }
+
 $(".setExercises").click(function(event){
     event.preventDefault();
     var searchIDs = $("#exerciselist input:checkbox:checked").map(function(){
@@ -140,51 +98,13 @@ $(".setExercises").click(function(event){
      }); 
     });
 });
-/*
-$('#exerciselist').on('click', '.excSelect', function() {
-    
-     var currentRow=$(this).closest("tr");
-     var excName=currentRow.find("td:eq(0)").text();
-     exercisesChosen.push(excName);
-     console.log(exercisesChosen);
 
-    });
-    */
 var patientID = "";
 var exercisesChosen = [];
 
   function errData(err){
   console.log('error!');
   console.log(err);
-}
-//just says there's an error in the console if one comes up
+  }
 
 });
- 
-  
-
-  
-
-  /*
-  $("#patientlist").on("click", function(){
-var databaseRef =  ref.child('Testing');
-databaseRef.push({
-  Name: name
-     });
-     });
-  } */
-/*
-  $("#submitButton").on("click", function(){
-var databaseRef =  ref.child('Testing');
-databaseRef.push({
-  Name: name
-  for(i=0, i<exercisesChosen.length, i++) {
-  Exercises: exercisesChosen[i]
-  }
-     });
-  });
-
-   */
-    
-
-
